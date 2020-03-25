20fb99f187f79a8f573d15a4515adcf24ce32aea
okostudio <marek.suchanski@gmail.com>
2020-03-25 15:23:26 +1100

    nutritional info modal added

--- src/js/OurRange.js
+++ src/js/OurRange.js
@@ -8,10 +8,18 @@ import ScrollReveal from "./utils/ScrollReveal";
 import OurRangeParticles from "./components/OurRangeParticles";
 import history from "./history";
 import { Link } from "react-router-dom";
+import OurRangeIcons from "./components/OurRangeIcons";
 
 const OurRange = props => {
     const data = ProductsData[0];
     const baseUrl = "/our-range/";
+    const [modalShowing, setModalShowing] = useState(false);
+    const [modalInfo, setModalInfo] = useState({
+        type: "info",
+        heading: "Nutritional Info",
+        image: "",
+        copy: null
+    });
     const [pathname, setPathname] = useState(window.location.pathname);
     const [current, setCurrent] = useState({
         group: null,
@@ -62,10 +70,44 @@ const OurRange = props => {
         return obj;
     };
 
+    // Show Modal
+    const showModal = modalType => {
+        console.log("SHOW MODAL");
+
+        setModalShowing(true);
+        // blurContainer.current.classList.add("blurred");
+        // modal.current.classList.add("isActive");
+        switchModal(modalType);
+    };
+    const switchModal = modalType => {
+        if (modalType === "info") {
+            setModalInfo({
+                type: "info",
+                heading: "Nutritional Info",
+                image: current.product.nutritionalInfo.image,
+                copy: null
+            });
+        } else {
+            setModalInfo({
+                type: "pairings",
+                heading: "Flavour Pairings",
+                image: current.product.flavourPairing.image,
+                copy: current.product.flavourPairing.copy
+            });
+        }
+    };
+    const closeModal = () => {
+        console.log("HIDE MODAL");
+        setModalShowing(false);
+        // blurContainer.current.classList.remove("blurred");
+        // modal.current.classList.remove("isActive");
+    };
+
+    //
+    // Extract current product from URL.
     useEffect(() => {
         console.log("--> Getting current product");
         setCurrent({ ...getCurrentProduct() });
-        // console.log("rendering:", ProductsData, "caca", current.product);
     }, [pathname]);
 
     const navigateTo = (url, direction = "") => {
@@ -78,58 +120,98 @@ const OurRange = props => {
     return (
         <ScrollReveal>
             <div className="page-wrappers our-range">
-                {ProductsData && current.group ? (
-                    <React.Fragment>
-                        <section className="DiscoverTheRange">
-                            <OurRangeParticles
-                                data={ProductsData[0]}
-                                current={current}
-                                onClickFunction={navigateTo}
-                            />
-                        </section>
-                        <section className="range-nav">
-                            <div className="container">
-                                {data.nav.map((item, i) => {
-                                    return (
-                                        <NavItem
-                                            {...item}
-                                            rangeUrl={baseUrl}
-                                            onClickFunction={navigateTo}
-                                            pathname={pathname}
-                                            key={3000 + i}
-                                        />
-                                    );
-                                })}
+                <div
+                    className={`blur-container ${
+                        modalShowing ? "blurred" : ""
+                    }`}
+                >
+                    {ProductsData && current.group ? (
+                        <React.Fragment>
+                            <section className="DiscoverTheRange">
+                                <OurRangeParticles
+                                    data={ProductsData[0]}
+                                    current={current}
+                                    onClickFunction={navigateTo}
+                                    showModal={showModal}
+                                />
+                            </section>
+                            <section className="range-nav">
+                                <div className="container">
+                                    {data.nav.map((item, i) => {
+                                        return (
+                                            <NavItem
+                                                {...item}
+                                                rangeUrl={baseUrl}
+                                                onClickFunction={navigateTo}
+                                                pathname={pathname}
+                                                key={3000 + i}
+                                            />
+                                        );
+                                    })}
+                                </div>
+                            </section>
+                            <div className="hidden">
+                                {current.products
+                                    ? current.products.map((p, i) => {
+                                          return (
+                                              <Link
+                                                  to={
+                                                      baseUrl +
+                                                      current.group +
+                                                      "/" +
+                                                      encodeURI(p.title)
+                                                  }
+                                                  key={9000 + i}
+                                              />
+                                          );
+                                      })
+                                    : ""}
                             </div>
-                        </section>
-                        <div className="hidden">
-                            {current.products
-                                ? current.products.map((p, i) => {
-                                      return (
-                                          <Link
-                                              to={
-                                                  baseUrl +
-                                                  current.group +
-                                                  "/" +
-                                                  encodeURI(p.title)
-                                              }
-                                              key={9000 + i}
-                                          />
-                                      );
-                                  })
-                                : ""}
-                        </div>
-                    </React.Fragment>
-                ) : null}
+                        </React.Fragment>
+                    ) : null}
+                </div>
 
-                <Helmet>
-                    <title>{"Red Rock Deli®"}</title>
-                    <meta
-                        name="description"
-                        content={"Red Rock Deli® - Feed Your Curiosity™"}
-                    />
-                </Helmet>
+                <div
+                    className={`modal ${modalInfo.type} ${
+                        modalShowing ? "isActive" : ""
+                    }`}
+                >
+                    <div className="info">
+                        <div className="black-box">
+                            <div
+                                className="header"
+                                onClick={() => {
+                                    closeModal();
+                                }}
+                            >
+                                <h3>{modalInfo.heading}</h3>
+                            </div>
+                            <img
+                                src={modalInfo.image}
+                                alt={modalInfo.heading}
+                            />
+                            {modalInfo.copy ? (
+                                <div
+                                    className="copy"
+                                    dangerouslySetInnerHTML={{
+                                        __html: modalInfo.copy
+                                    }}
+                                />
+                            ) : (
+                                ""
+                            )}
+                        </div>
+                        <OurRangeIcons onClickFunction={switchModal} />
+                    </div>
+                </div>
             </div>
+            <Helmet>
+                <title>{"Red Rock Deli®"}</title>
+                <meta
+                    name="description"
+                    content={"Red Rock Deli® - Feed Your Curiosity™"}
+                />
+            </Helmet>
         </ScrollReveal>
     );
 };
@@ -149,7 +231,7 @@ const NavItem = props => {
                 props.onClickFunction(props.baseUrl);
             }}
         >
-            <img src={props.imageUrl} />
+            <img src={props.imageUrl} alt="product" />
             <h3>{props.label}</h3>
             <Link className="hidden" to={props.rangeUrl + props.baseUrl} />
         </div>
